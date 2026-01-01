3d06b81583e83420e5b09df855ee95f7e5ddeaaf
Ani tech <43325329+vistemur@users.noreply.github.com>
2026-01-01 14:42:57 +0300

    Update survival upgrades and player initialization

--- src/sketch.js
+++ src/sketch.js
@@ -81,10 +81,10 @@ function survivalCucle() {
 
 function restartSurvival() {
     stop();
-    surv = new survival();
-    surv.getUpgrades();
-    currentBackground = int(random(0, backgrounds.length - 0.1));
     players.push(new player(38, 40, 32, width / 10 * 9 - 50, width / 10 * 9 - 100));
+    surv = new survival();
+    surv.getVagonUpgrades();
+    currentBackground = int(random(0, backgrounds.length - 0.1));
 }
 
 function startPve() {
